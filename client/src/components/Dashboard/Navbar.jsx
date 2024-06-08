a1c9ae1d243a76c13c407456f33406538c3ee6e2
pravesh meena <pravesh.meena2892@gmail.com>
2024-06-08 19:59:40 +0530

    first commit

---
+++ client/src/components/Dashboard/Navbar.jsx
@@ -0,0 +1,85 @@
+import React from "react";
+import {
+  Box,
+  Flex,
+  Container,
+  Input,
+  IconButton,
+  Avatar,
+  Menu,
+  MenuButton,
+  MenuList,
+  MenuItem,
+  Badge,
+  useColorMode,
+  useToast,
+} from "@chakra-ui/react";
+import { ChevronDownIcon, BellIcon, MoonIcon, SunIcon } from "@chakra-ui/icons";
+
+const Navbar = () => {
+  const { colorMode, toggleColorMode } = useColorMode();
+  const toast = useToast();
+
+  const handleLogout = () => {
+    toast({
+      title: "Logged Out",
+      description: "You have been successfully logged out.",
+      status: "success",
+      duration: 2000,
+      isClosable: true,
+    });
+
+    setTimeout(() => {
+      window.location.href = "/";
+    }, 2000);
+  };
+  return (
+    <Box
+      py={2}
+      backgroundColor="teal.500"
+      position="sticky"
+      top={0}
+      zIndex={100}
+    >
+      <Container maxW="container.lg">
+        <Flex justify="space-between" align="center">
+          <Box w="80%">
+            <Input
+              type="text"
+              placeholder="Search..."
+              size="sm"
+              borderRadius="full"
+              bg={colorMode === "light" ? "white" : "gray.800"}
+              px={4}
+              py={1}
+              color={colorMode === "light" ? "gray.800" : "white"}
+              _placeholder={{
+                color: colorMode === "light" ? "gray.500" : "gray.300",
+              }}
+              _focus={{ outline: "none" }}
+            />
+          </Box>
+          <Box>
+            <Flex align="center">
+              <IconButton
+                aria-label="Toggle Theme"
+                icon={colorMode === "light" ? <MoonIcon /> : <SunIcon />}
+                bg="transparent"
+                border="none"
+                onClick={toggleColorMode}
+              />
+              <Avatar
+                size="sm"
+                // add the image link here
+                src=""
+                ml={3}
+              />
+            </Flex>
+          </Box>
+        </Flex>
+      </Container>
+    </Box>
+  );
+};
+
+export default Navbar;
